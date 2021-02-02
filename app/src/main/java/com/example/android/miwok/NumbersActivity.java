811665468b91c0fc53a5a96bc0fd01d0c249694d
Rishabh malik <rishabhmalikeg@gmail.com>
2021-02-02 17:42:44 +0530

    arrayadapter

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,14 @@ package com.example.android.miwok;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -24,5 +32,26 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+
+    ArrayList<String> num = new ArrayList<String>();
+    num.add("one");
+    num.add("two");
+    num.add("three");
+    num.add("four");
+    num.add("five");
+    num.add("six");
+    num.add("seven");
+    num.add("eight");
+    num.add("nine");
+    num.add("ten");
+    Log.v("myactivity",num.get(1));
+    ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, num);
+
+    GridView listView = (GridView) findViewById(R.id.list);
+
+    listView.setAdapter(itemsAdapter);
     }
+
+
 }
